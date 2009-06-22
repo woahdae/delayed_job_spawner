79db3136554ea660eb5b3de6c140629a884c83eb
Woody Peterson <woody@CCiMacDev.local>
2009-06-22 16:55:23 -0700

    fixing install script

--- install.rb
+++ install.rb
@@ -1,4 +1,4 @@
 require 'rubygems'
-require 'file_utils'
+require 'fileutils'
 FileUtils.cp(File.dirname(__FILE__) + '/script/job_spawner', File.dirname(__FILE__) + "/../../../script/job_spawner")
 FileUtils.chmod "+x", File.dirname(__FILE__) + "/../../../script/job_spawner"
